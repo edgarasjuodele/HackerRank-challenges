f2ab7e6b77fa48ffdeefd7dc4a676e04768d1f47
Edgaras Juodele <70674020+qualitymango@users.noreply.github.com>
2020-09-11 08:19:55 +0100

    Solution for the Jumping on the Clouds problem
    https://www.hackerrank.com/challenges/jumping-on-the-clouds/problem

---
+++ JumpingOnTheClouds.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solve(vector<int> c){
+	int count = 0, index = 0;
+	while(index < n-1){
+           if(c[index+2] != 1){
+               index += 2;
+               count++;
+        } else if(c[index+1] != 1){
+            index += 1;
+            count++;
+        }
+    }
+    cout << count; 
+}
+
+int main ()
+{
+    int n;
+    cin >> n;
+    vector<int> c(n);
+    for(int i = 0; i < n; i++){
+        cin >> c[i];    
+    }   
+    solve(c);
+}
+
